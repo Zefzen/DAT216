eb0255527b2b6f1931cb3a9cd867dd09f364ac2b
Bosolaj <divad.sjoberg@gmail.com>
2024-05-14 12:13:19 +0200

    scrollande produktgrid

--- resources/resources/iMatApp/iMatApp/src/imat/MainViewController.java
+++ resources/resources/iMatApp/iMatApp/src/imat/MainViewController.java
@@ -4,15 +4,22 @@ package imat;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 public class MainViewController implements Initializable {
 
     @FXML
     Label pathLabel;
+    @FXML
+    private GridPane productList;
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
@@ -21,6 +28,21 @@ public class MainViewController implements Initializable {
         String iMatDirectory = iMatDataHandler.imatDirectory();
 
         pathLabel.setText(iMatDirectory);
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 4; j++) {
+                productItem product = new productItem("Product " + (i * 5 + j + 1));
+
+                productList.add(product, j, i);
+            }
+
+        }
+
+
+
+
+
 
     }
 
